6275201e58280ec028d36128b9a4900f8e016bca
Taulant Ibraj <tibraj94@gmail.com>
2020-04-14 15:43:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,6 +6,11 @@ class Anagram
     @word = word
   end
 
-  def self.matches
+  def match(array)
+    matches = []
+    array.each do |word|
+      matches << word if word.split('').sort == self.word.split('').sort
+    end
+    matches
   end
 end
